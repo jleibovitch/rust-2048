500e79bc5c1a8acec399f82a77394056b29f802c
Jamieson Leibovitch <jamieson.leibovitch@uoit.net>
2020-02-15 13:08:00 -0500

    game displays win/loss

--- src/main.rs
+++ src/main.rs
@@ -7,7 +7,6 @@ use display::Display;
 
 fn main() {
     let mut game_grid = grid::Grid::new();
-    println!("{:?}", game_grid);
 
     let game_display = display::cli_display::CLIDisplay::new();
     game_display.render(&game_grid);
@@ -18,6 +17,20 @@ fn main() {
             break
         }
         game_display.render(&game_grid);
+
+        let (game_over, win) = game_grid.game_over();
+        if game_over {
+
+            if win {
+                println!("\n\nYou won!");
+            } else {
+                println!("\n\nYou lose..");
+            }
+
+            break;
+        }
+
+       
     }
     
 }
